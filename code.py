ad7e0c818b9f93e8a6c8c9682307705610cc8c83
Krish Gupta <73186767+Kr1sh-gupta@users.noreply.github.com>
2024-06-05 21:29:44 +0530

    Update code.py

--- code.py
+++ code.py
@@ -2,11 +2,15 @@ from urllib.request import urlopen
 from urllib.parse import quote_plus
 
 
-//Enter the url that you wanna scrape
+#Enter the url that you wanna scrape
 url = quote_plus('https://github.com/')
 
 
-//you can also get the api by logging --> https://crawlbase.com/ 
+#you can also get the api by logging --> https://crawlbase.com/ 
+# Tospecify which data need to be scrapped just write an attribute
+# Example: &scraper=product-reviews
+
+## To beautify the data recieved &pretty=true
 handler= urlopen('https://api.crawlbase.com/?token=Your_Api_Here&url=' +url)
 
 print(handler.read().decode('utf-8'))
